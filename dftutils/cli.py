105838113d501ba4080331abf2ca93f49e407bb5
Jooper <alexandredmfs@gmail.com>
2024-07-08 11:15:38 +0100

    Added output of structures

--- dftutils/cli.py
+++ dftutils/cli.py
@@ -295,6 +295,14 @@ def strain(path, strain, scan, min_strain, max_strain, number, scan_output, conf
     type=int,
 )
 @click.option(
+    "--output-path",
+    "-o",
+    help="Path to output the neb directories.",
+    required=False,
+    default=None,
+    type=click.Path(exists=True, dir_okay=False),
+)
+@click.option(
     "--config",
     "-conf",
     help="Config file for advanced settings.",
@@ -302,7 +310,7 @@ def strain(path, strain, scan, min_strain, max_strain, number, scan_output, conf
     type=click.Path(exists=True, dir_okay=False),
     show_default=True,
 )
-def neb(path, initial, final, number, config):
+def neb(path, initial, final, number, output_path, config):
     user_settings = loadfn(config) if config is not None else {}
     func_args = list(locals().keys())
 
@@ -323,8 +331,10 @@ def neb(path, initial, final, number, config):
 
     if not path is None:
         neb = Neb.from_path(path, number)
+        neb.to_path(output_path)
     elif path is None and not initial is None and not final is None:
         neb = Neb.from_initial_and_final(initial, final, number)
+        neb.to_path(output_path)
     else:
         raise Exception(
             f"Path or initial and final images were not defined."
