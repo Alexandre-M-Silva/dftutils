80117e19286b019f9b823dc33c62007d75583876
Alexandre-M-Silva <alexandredmfs@gmail.com>
2024-10-05 13:10:58 +0100

    Redid the polarization branch finding code

--- dftutils/polarization.py
+++ dftutils/polarization.py
@@ -59,31 +59,70 @@ def polarization_from_path(path, bmin=-5, bmax=5):
 
     return df
 
-class PolarizationPlotter:
+def polarization_branch(data, axis, start, bias=0):
+    n = data.iloc[:, 0].nunique()
+    P = np.zeros(n)
+    P[0] = start
+    for i in range(0, n-1):
+        values = data[data["Image"]==i+1].iloc[:, axis+1].values
+        dists = np.abs((values - bias) - P[i]) 
+        closest_at = np.argmin(dists)
+        P[i+1] = values[closest_at]
+    
+    return P
+
+def polarization_branch_derivative_bias(branch):
+    grad = np.gradient(branch)
+    return np.sum(grad) / len(grad)
+
+class Polarization:
     def __init__(self, path=None):
         self.path = path
         self.data = polarization_from_path(path)
         self.branches = None
         self.switch = None
-    
-    def get_spontaneous(self):
+
+    def _calc_branches_and_switch(self, axis=2):
+        if self.branches is None:
+            starts = self.data[self.data["Image"] == 0].iloc[:, axis+1]
+            self.branches = [polarization_branch(self.data, axis, start) for start in starts]
+            branches_bias = [polarization_branch_derivative_bias(branch) for branch in self.branches]
+            self.branches = [polarization_branch(self.data, axis, start, bias) for start, bias in zip(starts, branches_bias)]
+
+        if self.switch is None:
+            abs_means = [np.abs(np.mean(branch)) for branch in self.branches]
+            i = np.argmin(abs_means)
+            self.switch = self.branches[i]  
+
+    def get_spontaneous(self, axis=2):
+        self._calc_branches_and_switch(axis)
         return 0.5*(self.switch[-1] - self.switch[0])
     
-    def plot(self, ylim=None, save=True):
+class PolarizationPlotter:
+    def __init__(self, pol):
+        self.pol = pol
+
+    def from_path(path):
+        pol = Polarization(path)
+        return PolarizationPlotter(pol)
+    
+    def from_pol(pol):
+        return PolarizationPlotter(pol)
+    
+    def plot(self, axis=2, save=True):
         plt.style.use(os.path.join(os.path.dirname(__file__), 'dftutils.mplstyle'))
 
         fig, ax = plt.subplots()
         ax.set_xlabel('Image')
         ax.set_ylabel(r'$P_s\ (\mu C/cm^2)$')
-        ax.scatter(self.data['Image'], self.data.iloc[:, 3], s=0.5, color='black')
+        ax.scatter(self.pol.data['Image'], self.pol.data.iloc[:, axis+1], s=0.5, color='black')
     
-        if not ylim is None:
-            #self.branches = branches_from_polarization(self.data)
-            #self.switch = midpoint_branch_from_branches(self.branches)
-            ax.set_ylim(ylim)
+        self.pol._calc_branches_and_switch(axis)
+        for b in self.pol.branches:
+            ax.plot(b, "o-")
 
         if save:
-            fig.savefig(os.path.join(self.path, 'plot.png'), bbox_inches='tight', pad_inches=0.05)
+            fig.savefig(os.path.join(self.pol.path, 'plot.png'), bbox_inches='tight', pad_inches=0.05)
 
         return fig, ax
 
